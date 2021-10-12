ecf71783ea49fa8aecff290dd2afceec58739015
Denisgby <denis.gby@gmail.com>
2021-10-12 23:21:16 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
  // не забудьте установить для кнопки в html-е значение атрибута id='login'
- var loginButton = document.getElementById('buttonLogInId');
+ var loginButton = document.getElementById('login');
  loginButton.onclick = function() {
  
     // вот здесь ваш код.
      var emailE1=document.getElementById('emailInputId');
      var passE1=document.getElementById('passInputId');
      var checkE1=document.getElementById('inputCheckBoxId');
-     var buttonE1=document.getElementById('buttonLogInId');
+     var buttonE1=document.getElementById('login');
      
      //console.log(emailE1.value)
      //console.log(passE1.value)
-     //console.log(checkE1.value)
+     console.log(checkE1.value)
      //console.log(buttonE1.value)
-    window.alert('email: ' + emailE1.value +'; '+'Password: '+ passE1.value +';')
+    window.alert('Email: ' + emailE1.value +'; '+'Password: '+ passE1.value +';')
  };
